727ffebfad2dd1e2df250cfdd8d14a927751708c
Dumble009 <daichi19990821@gmail.com>
2022-07-21 21:21:24 +0900

    use led as output

--- sketch_interactive/sketch_interactive.ino
+++ sketch_interactive/sketch_interactive.ino
@@ -2,18 +2,32 @@
 Servo myservo;
 const int pin = 9;
 int val;
-String incomingByte;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   myservo.attach(pin);
+  pinMode(13, OUTPUT);
 }
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available() > 0){
-    incomingByte = Serial.readStringUntil('\n');
-    val = incomingByte.toInt();
+    delay(20);
+    val = 0;
+    while(Serial.available() > 0){
+      char c = Serial.read();
+      if(c == '\n'){
+        break;
+      }
+      val *= 10;
+      val += c - '0';
+    }
+      Serial.println(val);
+  digitalWrite(13, HIGH);
+  delay(val * 1000);
+  }else{
+    
+  digitalWrite(13, LOW);
   }
-  myservo.write(val);
-  delay(15);
+  //myservo.write(val);
+  //delay(15);
 }
