d858fb53b3cca2803d852d145cd46e2949a82ae7
Dumble009 <daichi19990821@gmail.com>
2022-07-21 20:41:24 +0900

    it works but not stable

--- sketch_interactive/sketch_interactive.ino
+++ sketch_interactive/sketch_interactive.ino
@@ -1,9 +1,19 @@
+#include <Servo.h>
+Servo myservo;
+const int pin = 9;
+int val;
+String incomingByte;
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  myservo.attach(pin);
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(Serial.available() > 0){
+    incomingByte = Serial.readStringUntil('\n');
+    val = incomingByte.toInt();
+  }
+  myservo.write(val);
+  delay(15);
 }
